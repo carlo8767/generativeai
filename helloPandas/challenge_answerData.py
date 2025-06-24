69ffaa37765945a333d128028f2c087cd3f292d7
carlo8767 <carlo8767@gmail.com>
2025-06-24 16:18:36 +0200

    series_command

---
+++ helloPandas/challenge_answerData.py
@@ -0,0 +1,42 @@
+import pandas as ps
+
+
+
+
+if __name__ == '__main__':
+    filename = "olympics_1896_2004.csv"
+    oo = ps.read_csv(filename, skiprows=5)
+
+    # WHAT IS THE TIME RANGE COVERED IN THIS DATA SET?
+    range_year = oo.Year.cummax().unique()
+    size_year = range_year[len(range_year)-1]-range_year[0]
+    print(f'The range is between {range_year[0]} and {range_year[len(range_year)-1]}')
+    # WHAT ARE THE MISSING OLYMPICS YEAR?
+    begin_year = range_year[0]
+    for i, v in enumerate(range_year):
+
+        if i == 0:
+            continue
+        else:
+          begin_year += 4
+          if begin_year == v:
+                continue
+          else:
+
+              while True:
+                  if begin_year == v:
+                      begin_year = v
+                      break
+                  print(f'The missing year are {begin_year}')
+                  begin_year += 4
+
+    # WHAT ARE THE TYPE OF MEDAL AWARDED?
+    for v in oo.Medal.unique():
+        print(f'The medal awarded are {v}')
+    # ACROSS ALL THE OLYMPIC GAMES HOW MANY GOLD SILVER, AND BRONZE MODEL HAVE THERE BEEN?
+    print(f'These are the number of medal {oo.Medal.value_counts()}')
+    # WHAT ARE THE VARIOUS NATIONAL OLIMPIC COMMIT?
+    print(oo.NOC.unique())
+    # WHAT DOES THE NOC "ZZX" represent?
+    print(oo.columns)
+
--- helloPandas/hello.py
+++ helloPandas/hello.py
@@ -8,21 +8,31 @@ if __name__ == '__main__':
   # THERE ARE MULTIPLE COMMAND FOR PANDAS https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html # apt update; apt install git
 
   file = ps.read_csv(filename, skiprows=5)
+  print(file.columns)
+  print(file.Year.unique())
 
-  # 2. ATTRIBUTE
-  tuples = file.shape
-  file.info()
-
-  print(file.describe())
 
   """
   OTHER USEFUL METHOD
+  file.sample() random 3 row
   print(file.sample(5)) # RANDOMLY SHOW ROW
   print(file.head(3)) # SHOW HEAD
   print(file.tail) # SHOW FROM THE BUTTON
   file.info() SEE INFORMATION OF  THE DATA TYPE
   print(file.describe()) # STATISTICAL VALUE
    ps.get_option # SET UP VISUALIZATION ROW NUMBER OF COLUMSN
+  """
+
+  """
+  SELECT A SERIES I CAN ACCESS DIRECTLY WITH AN ATTRIBUTE
+   print(file.columns) VISUALIZE ALL THE COLUMNS
+  print(file.Year.unique()) # PRINT UNIQUE
+  print(file.columns)
+  for s in file.columns:
+  print(file[s].unique())
+  print(file.Year.value_counts(normalize=True)) # COUNT 
+  oo.Medal.value_counts() # COUNT
+
   """
 
   # SEP separate the file for a specific char
